3ef6a594da7b08a0aa2b5684d724ae96c9b18f69
Ryan Davis <ryepup@gmail.com>
2022-12-11 21:59:48 +0000

    day 4 part 1
    I think there's a macro hiding in here for parsing the file into a particular shape

---
+++ src/day4.lisp
@@ -0,0 +1,27 @@
+(in-package :advent-2022)
+
+(defstruct (section-assignment (:conc-name sa-))
+  (start 0 :type integer :read-only t)
+  (end 0 :type integer :read-only t))
+
+(defmethod contains ((a section-assignment) (b section-assignment))
+  (and (<= (sa-start a) (sa-start b))
+       (>= (sa-end a) (sa-end b))))
+
+(defun parse-section-assignment (s)
+  (let ((idx (position #\- s)))
+    (make-section-assignment
+      :start (parse-integer s :start 0 :end idx)
+      :end (parse-integer s :start (+ idx 1)))))
+
+(defun parse-assignment (line)
+  (let ((idx (position #\, line)))
+    (values
+      (parse-section-assignment (subseq line 0 idx))
+      (parse-section-assignment (subseq line (+ idx 1))))))
+
+(defun day4-1 ()
+  (iter (for line in-file "src/day4.txt" using #'read-line)
+    (for (values a b) = (parse-assignment line))
+    (counting (or (contains a b)
+                  (contains b a)))))
