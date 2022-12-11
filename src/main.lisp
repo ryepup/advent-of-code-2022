b38d78d8cf0901ce67f396547bf63372101cea6e
Ryan Davis <ryepup@gmail.com>
2022-12-11 16:56:30 +0000

    day 1 part 2
    Pulled the elf reading out, then used some basic sequence manipulation
    to get the rest.

--- src/main.lisp
+++ src/main.lisp
@@ -3,11 +3,18 @@
 
 (in-package :advent-2022)
 
-(defun most-calories ()
+(defun elf-calories ()
   (iter (for line in-file "src/day1.txt" using #'read-line)
     (for calories = (parse-integer line :junk-allowed t))
     (if calories
         (summing calories into elf)
         (prog1
-            (maximize elf)
+            (collect elf)
           (setf elf 0)))))
+
+
+(defun most-calories ()
+  (apply #'max (elf-calories)))
+
+(defun sum-of-top-3 ()
+  (reduce #'+ (subseq (sort (elf-calories) #'>) 0 3)))
