b35ceddfeb38d9a90c2bbe007a02cf56fc479c41
Ryan Davis <ryepup@gmail.com>
2022-12-11 20:47:39 +0000

    day 3 part 2
    Still very iterative, not super clear about `on` vs `in` but it gave me the result I wanted.

--- src/day3.lisp
+++ src/day3.lisp
@@ -1,5 +1,9 @@
 (in-package :advent-2022)
 
+(defun readlines (path)
+  (iter (for line in-file path using #'read-line)
+    (collect line)))
+
 (defun read-rucksacks ()
   (iter (for line in-file "src/day3.txt" using #'read-line)
     (for n = (/ (length line) 2))
@@ -14,4 +18,10 @@
 (defun day3-1 ()
   (iter (for (a . b) in (read-rucksacks))
     (for item = (first (intersection (coerce a 'list) (coerce b 'list))))
+    (sum (priority item))))
+
+(defun day3-2 ()
+  (iter (for (a b c) on (readlines "src/day3.txt") by #'cdddr) ; three rows at once
+    (for item = (first (intersection (coerce a 'list)
+                                     (intersection (coerce b 'list) (coerce c 'list)))))
     (sum (priority item))))
