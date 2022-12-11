facb0b0b5efc3395787e5d1e92f0c9cefd14172b
Ryan Davis <ryepup@gmail.com>
2022-12-11 17:40:48 +0000

    day 2 part 1
    pretty straightforward parsing

--- src/main.lisp
+++ src/main.lisp
@@ -18,3 +18,30 @@
 
 (defun sum-of-top-3 ()
   (reduce #'+ (subseq (sort (elf-calories) #'>) 0 3)))
+
+(defun decode (c)
+  (ecase c
+    ((#\A #\X) :rock)
+    ((#\B #\Y) :paper)
+    ((#\C #\Z) :scissors)))
+
+(defun score (them us)
+  (+ (ecase us
+       (:rock 1)
+       (:paper 2)
+       (:scissors 3))
+     (cond
+      ((eq them us) 3)
+      ((or (and (eq them :rock) (eq us :paper))
+           (and (eq them :paper) (eq us :scissors))
+           (and (eq them :scissors) (eq us :rock))) 6)
+      (t 0))))
+
+(defun read-guide ()
+  (iter (for line in-file "src/day2.txt" using #'read-line)
+    (collect (cons (decode (char line 0))
+                   (decode (char line 2))))))
+
+(defun total-score ()
+  (iter (for (them . us) in (read-guide))
+    (sum (score them us))))
